2b1a2427fee5f9bb25b7354a03d92307210b84f0
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-08-07 10:36:12 +0500

    #WB-1 Создание проекта

---
+++ Web+App(ASP.NET Core MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_App_ASP.NET_Core_MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
