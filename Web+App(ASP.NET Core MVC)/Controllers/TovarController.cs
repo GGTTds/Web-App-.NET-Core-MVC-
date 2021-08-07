c9c0f7db57366370edcb7b2541b1d322cad8c821
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-08-07 18:33:46 +0500

    #WB-9 Добавление вывода имеющегося товара => TovarController.cs(Index.cshtml)

--- Web+App(ASP.NET Core MVC)/Controllers/TovarController.cs
+++ Web+App(ASP.NET Core MVC)/Controllers/TovarController.cs
@@ -4,84 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_App_ASP.NET_Core_MVC_.Models;
 
 namespace Web_App_ASP.NET_Core_MVC_.Controllers
 {
     public class TovarController : Controller
     {
         // GET: TovarController
-        public ActionResult Index()
+        private TaxiInDronContext db;
+        public TovarController(TaxiInDronContext context)
         {
-            return View();
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Drons.ToListAsync());
         }
 
-        // GET: TovarController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: TovarController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TovarController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TovarController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TovarController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TovarController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: TovarController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
